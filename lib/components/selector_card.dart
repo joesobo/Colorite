d077ff56ccd87668468aeaeab3137cf5dfa55bcf
Joe Soboleski <soboleskijoe@gmail.com>
2019-08-01 19:17:07 -0700

    finished main color selector card

--- lib/components/selector_card.dart
+++ lib/components/selector_card.dart
@@ -9,17 +9,37 @@ class SelectorCard extends StatefulWidget {
   final int r = 125;
   final int g = 125;
   final int b = 125;
+  final int h = 180;
+  final int s = 50;
+  final int v = 50;
+
+  final Color mainColor = Colors.white;
 
   @override
-  _SelectorCardState createState() => _SelectorCardState(r: r, g: g, b: b);
+  _SelectorCardState createState() => _SelectorCardState(
+      r: r, g: g, b: b, h: h, s: s, v: v, mainColor: mainColor);
 }
 
 class _SelectorCardState extends State<SelectorCard> {
   int r;
   int g;
   int b;
+  int h;
+  int s;
+  int v;
+  Color mainColor;
 
-  _SelectorCardState({this.r, this.g, this.b});
+  String hexText = '';
+
+  _SelectorCardState(
+      {this.r, this.g, this.b, this.h, this.s, this.v, this.mainColor});
+
+  @override
+  void initState() {
+    mainColor = Color.fromRGBO(r, g, b, 1);
+    hexText = 'Hex: #${mainColor.value.toRadixString(16).toUpperCase()}';
+    super.initState();
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -27,45 +47,118 @@ class _SelectorCardState extends State<SelectorCard> {
       shape: RoundedRectangleBorder(
           borderRadius: BorderRadius.all(Radius.circular(20.0))),
       title: Text(
-        'Edit your main color\n $r, $g, $b',
+        'Edit your main color',
         textAlign: TextAlign.center,
       ),
       content: Column(
         crossAxisAlignment: CrossAxisAlignment.stretch,
         mainAxisAlignment: MainAxisAlignment.spaceBetween,
         children: <Widget>[
+          //hex text, color indicator, and mode selector
           Column(
             children: <Widget>[
-              ColorIndicator(color: Color.fromRGBO(r, g, b, 1)),
-              ModeSelector(),
+              //hex text
+              Text(hexText, style: TextStyle(color: Colors.grey)),
+              //color indicator
+              mode == Mode.rgb
+                  ? ColorIndicator(color: Color.fromRGBO(r, g, b, 1))
+                  : ColorIndicator(
+                      color:
+                          HSVColor.fromAHSV(1, h.toDouble(), s / 100, v / 100)
+                              .toColor()),
+              //mode selector
+              ModeSelector(parent: this),
             ],
           ),
-          Column(
-            children: <Widget>[
-              ColorSlider(parent: this, text: 'Red', value: r, activeColor: Colors.red, overlayColor: Color(0x5fe57373),),
-              ColorSlider(parent: this, text: 'Green', value: g, activeColor: Colors.green, overlayColor: Color(0x5f81c784),),
-              ColorSlider(parent: this, text: 'Blue', value: b, activeColor: Colors.blue, overlayColor: Color(0x5f03A9f4),),
-            ],
-          ),
+          //color selection sliders
+          mode == Mode.rgb ? rgbSliders() : hsvSliders(),
+          //submit button
           RaisedButton(
             color: Colors.blueGrey,
             elevation: 5,
-            splashColor: Colors.lightBlue,
+            splashColor: Colors.blueGrey[300],
+            shape: RoundedRectangleBorder(
+              borderRadius: BorderRadius.circular(5),
+            ),
             child: Text(
               'Save',
               style: defaultText,
             ),
             onPressed: () {
-              //Navigator.pop(context);
-              print(r);
+              Navigator.pop(context);
             },
           ),
         ],
       ),
     );
   }
+
+  //creates 3 sliders for rgb
+  Widget rgbSliders() {
+    return Column(
+      children: <Widget>[
+        ColorSlider(
+          parent: this,
+          text: 'Red',
+          value: r,
+          activeColor: Colors.red,
+          overlayColor: Color(0x5fe57373),
+          max: 255,
+        ),
+        ColorSlider(
+          parent: this,
+          text: 'Green',
+          value: g,
+          activeColor: Colors.green,
+          overlayColor: Color(0x5f81c784),
+          max: 255,
+        ),
+        ColorSlider(
+          parent: this,
+          text: 'Blue',
+          value: b,
+          activeColor: Colors.blue,
+          overlayColor: Color(0x5f03A9f4),
+          max: 255,
+        ),
+      ],
+    );
+  }
+
+  //creates 3 sliders for hsv
+  Widget hsvSliders() {
+    return Column(
+      children: <Widget>[
+        ColorSlider(
+          parent: this,
+          text: 'Hue',
+          value: h.toInt(),
+          activeColor: Colors.lightBlueAccent,
+          overlayColor: Color(0x5f4fc3f7),
+          max: 360,
+        ),
+        ColorSlider(
+          parent: this,
+          text: 'Saturation',
+          value: s.toInt(),
+          activeColor: Colors.white,
+          overlayColor: Color(0x5fffffff),
+          max: 100,
+        ),
+        ColorSlider(
+          parent: this,
+          text: 'Value',
+          value: v.toInt(),
+          activeColor: Colors.black,
+          overlayColor: Color(0x5f000000),
+          max: 100,
+        ),
+      ],
+    );
+  }
 }
 
+//display color
 class ColorIndicator extends StatelessWidget {
   final Color color;
 
@@ -74,7 +167,7 @@ class ColorIndicator extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Container(
-      margin: EdgeInsets.symmetric(vertical: 16),
+      margin: EdgeInsets.only(top: 8, bottom: 16),
       height: 104,
       decoration: BoxDecoration(
         borderRadius: BorderRadius.circular(5),
@@ -84,12 +177,12 @@ class ColorIndicator extends StatelessWidget {
   }
 }
 
-class ModeSelector extends StatefulWidget {
-  @override
-  _ModeSelectorState createState() => _ModeSelectorState();
-}
+//switches between rgb and hsv
+class ModeSelector extends StatelessWidget {
+  final _SelectorCardState parent;
 
-class _ModeSelectorState extends State<ModeSelector> {
+  ModeSelector({@required this.parent});
+
   @override
   Widget build(BuildContext context) {
     //creates row for selected color mode
@@ -98,26 +191,26 @@ class _ModeSelectorState extends State<ModeSelector> {
       children: <Widget>[
         //RGB outline vs filled
         mode == Mode.rgb
-            ? filled('RGB', () {
-                setState(() {
-                  mode = Mode.rgb;
-                });
-              }, Mode.rgb)
+            ? filled('RGB', Mode.rgb)
             : outline('RGB', () {
-                setState(() {
+                parent.setState(() {
                   mode = Mode.rgb;
+                  Color color = parent.mainColor;
+                  parent.r = color.red;
+                  parent.g = color.green;
+                  parent.b = color.blue;
                 });
               }, Mode.rgb),
         //HSV outline vs filled
         mode == Mode.hsv
-            ? filled('HSV', () {
-                setState(() {
-                  mode = Mode.hsv;
-                });
-              }, Mode.hsv)
+            ? filled('HSV', Mode.hsv)
             : outline('HSV', () {
-                setState(() {
+                parent.setState(() {
                   mode = Mode.hsv;
+                  HSVColor color = HSVColor.fromColor(parent.mainColor);
+                  parent.h = color.hue.toInt();
+                  parent.s = (color.saturation * 100).toInt();
+                  parent.v = (color.value * 100).toInt();
                 });
               }, Mode.hsv),
       ],
@@ -143,11 +236,11 @@ class _ModeSelectorState extends State<ModeSelector> {
   }
 
   //returns filled button of mode selector
-  FlatButton filled(String text, Function onPressed, Mode mode) {
+  FlatButton filled(String text, Mode mode) {
     return FlatButton(
       color: Colors.grey,
       child: Text(text),
-      onPressed: onPressed,
+      onPressed: () {},
       shape: RoundedRectangleBorder(
         borderRadius: BorderRadius.only(
           topLeft: mode == Mode.rgb ? Radius.circular(5) : Radius.zero,
@@ -160,14 +253,23 @@ class _ModeSelectorState extends State<ModeSelector> {
   }
 }
 
+//creates a color slider based on mode
 class ColorSlider extends StatelessWidget {
   final _SelectorCardState parent;
   final String text;
   final int value;
   final Color activeColor;
   final Color overlayColor;
+  final double max;
 
-  ColorSlider({this.parent, this.text, this.value, this.activeColor, this.overlayColor});
+  ColorSlider({
+    @required this.parent,
+    this.text,
+    this.value,
+    this.activeColor,
+    this.overlayColor,
+    this.max,
+  });
 
   @override
   Widget build(BuildContext context) {
@@ -179,7 +281,9 @@ class ColorSlider extends StatelessWidget {
             mainAxisAlignment: MainAxisAlignment.spaceBetween,
             children: <Widget>[
               Text(text),
-              Text(value.toString()),
+              (text == 'Saturation' || text == 'Value')
+                  ? Text(value.toString() + "%")
+                  : Text(value.toString())
             ],
           ),
           SliderTheme(
@@ -192,17 +296,39 @@ class ColorSlider extends StatelessWidget {
             ),
             child: Slider(
               min: 0,
-              max: 255,
+              max: max,
               value: value.toDouble(),
               onChanged: (double newValue) {
                 parent.setState(() {
-                  if(text == 'Red'){
-                    parent.r = newValue.round();
-                  }
-                  else if(text == 'Blue'){
-                    parent.b = newValue.round();
-                  }else{
-                    parent.g = newValue.round();
+                  if (text == 'Red' || text == 'Blue' || text == 'Green') {
+                    parent.hexText =
+                        'Hex: #${Color.fromRGBO(parent.r, parent.g, parent.b, 1).value.toRadixString(16).toUpperCase()}';
+                    parent.mainColor =
+                        Color.fromRGBO(parent.r, parent.g, parent.b, 1);
+
+                    if (text == 'Red') {
+                      parent.r = newValue.round();
+                    } else if (text == 'Blue') {
+                      parent.b = newValue.round();
+                    } else if (text == 'Green') {
+                      parent.g = newValue.round();
+                    }
+                  } else if (text == 'Hue' ||
+                      text == 'Saturation' ||
+                      text == 'Value') {
+                    parent.hexText =
+                        'Hex: #${HSVColor.fromAHSV(1, parent.h.toDouble(), parent.s / 100, parent.v / 100).toColor().value.toRadixString(16).toUpperCase()}';
+                    parent.mainColor = HSVColor.fromAHSV(1, parent.h.toDouble(),
+                            parent.s / 100, parent.v / 100)
+                        .toColor();
+
+                    if (text == 'Hue') {
+                      parent.h = newValue.round();
+                    } else if (text == 'Saturation') {
+                      parent.s = newValue.round();
+                    } else if (text == 'Value') {
+                      parent.v = newValue.round();
+                    }
                   }
                 });
               },
